d971f9688303390dc599189d0d4ca57837c6a4d0
hayden.rear <hayden.rear@gmail.com>
2024-07-14 11:37:00 -0400

    updated

---
+++ src/test/java/com/hayden/jpapersistence/JpaPersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hayden.jpapersistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPersistenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
